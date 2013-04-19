6b9dff2fa75f57f19802589cfeb1269794565f12
Sandeep Manchem <Sandeep Manchem@SandeepManchem>
2013-04-18 20:10:20 -0400

    googleVoice

--- BuzzGuardian/src/guardian/GoogleVoice.java
+++ BuzzGuardian/src/guardian/GoogleVoice.java
@@ -1,102 +1,121 @@
 package guardian;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
 
-import org.dom4j.Document;
 import org.dom4j.DocumentException;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
 
 import com.techventus.server.voice.Voice;
+import com.techventus.server.voice.datatypes.records.SMS;
+import com.techventus.server.voice.datatypes.records.SMSThread;
 
+/**
+ * Google Voice API Services
+ */
 public class GoogleVoice {
 
-	String userName;
-	String pass;
-	Voice voice;
+	private static final String BUZZ_GUARDIAN_ADDRESS = "buzzguardian@gmail.com";
+	private static final String BUZZ_GUARDIAN_PASSWORD = "gtbuzz00";
 
-	GoogleVoice()
-	{
-		userName = "buzzguardian@gmail.com";
-		pass = "gtbuzz00";
+	private static Voice voice;
 
+	static {
 		try {
-			voice = new Voice(userName, pass);
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
+			voice = new Voice(BUZZ_GUARDIAN_ADDRESS, BUZZ_GUARDIAN_PASSWORD);
+		} catch (IOException e) {
+			System.err.println("ERROR NOT ABLE TO CONNECT TO GOOGLE VOICE");
+			e.printStackTrace();
 		}
 	}
+	
+	/**
+	 * @return Unread SMS messages in the form of a List of SMSObjects 
+	 * @throws DocumentException
+	 */
+	public static List<SMSObject> getUnreadSMS() throws DocumentException{
 
-	// Returns a list of strings. 3 consecutive strings represent a SMS
-	// First string is sender number
-	// Second string is sms text
-	// Third string is time
-	public ArrayList<String> getUnreadSMS() throws DocumentException {
+		List<SMSObject> smsList = new ArrayList<SMSObject>();
 
-		ArrayList<String> aList = new ArrayList<String>();
-
+		Collection<SMSThread> smsthreads = null;
+		
 		try {
-			String recentMsg = voice.getUnreadSMS();
-			// System.out.println( recentMsg );
-			recentMsg = recentMsg.replace("<![CDATA[", "");
-			recentMsg = recentMsg.replace("]]>", "");
-			recentMsg = recentMsg.replaceAll("&", "");
-
-			Document document = DocumentHelper.parseText(recentMsg);
-			List list = document.selectNodes("//div[@class='gc-message-sms-row']");
-			System.out.println(list.size());
-			int readCount = 0;
-
-			File fname = new File("readCount.txt");
-			if (!fname.exists()) {
-				PrintWriter writer = new PrintWriter("readCount.txt");
-				writer.println(readCount);
-				writer.close();
-			}
-			else {
-				Scanner sc = new Scanner( new File("readCount.txt") );
-				readCount = list.size() - sc.nextInt();
-				sc.close();
-				PrintWriter writer = new PrintWriter("readCount.txt");
-				writer.println(list.size());
-				writer.close();
-			}
-
-			Iterator msgIterator = list.iterator();
-			for (int i = 0; i < readCount && msgIterator.hasNext(); ++i) {
-				Element msgElement = (Element) msgIterator.next();
-				for (Iterator msgAttributeIterator = msgElement.elementIterator(); msgAttributeIterator.hasNext();) {
-					Element msgAttributeElement = (Element) msgAttributeIterator.next();
-					System.out.println(msgAttributeElement.attributeValue("class") + "\t" + msgAttributeElement.getStringValue().trim());
-					aList.add(msgAttributeElement.getStringValue().trim());
-				}
-			}
-		}
-		catch(Exception e) {
-			System.out.println(e.getMessage());
+			smsthreads = voice.getSMSThreads();
+		} catch (IOException e) {
+			System.err.println("ERROR GETTING SMS THREADS");
+			e.printStackTrace();
 		}
 
-		return aList;
+		int size = 0;
+		for (SMSThread t : smsthreads) {
+			Collection<SMS> sms = t.getAllSMS();
+			size += sms.size();
+			// System.out.println( "Thread Messages: " + sms.size() );
+		}
+
+		System.out.println("Current Messages: " + size);
+
+		int readCount;
+		File fname = new File("readCount.txt");
+		if (!fname.exists()) {
+			System.out.println("Initializing !!!");
+			readCount = 0;
+		} else {
+			Scanner sc = null;
+			try {
+				sc = new Scanner(new File("readCount.txt"));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			readCount = size - sc.nextInt();
+			sc.close();
+		}
+
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter("readCount.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		writer.println(size);
+		writer.close();
+
+		int kt = 0;
+		for (SMSThread t : smsthreads) {
+			if (kt++ >= readCount) {
+				break;
+			}
+			Collection<SMS> sms = t.getAllSMS();
+			for (SMS s : sms) {
+				System.out.println("Number: " + s.getFrom().getNumber());
+				System.out.println("Text: " + s.getContent());
+				System.out.println("Time: " + s.getDateTime().toString());
+
+				smsList.add(new SMSObject(s.getFrom().getNumber(), s
+						.getContent(), s.getDateTime().toString()));
+				break;
+			}
+		}
+
+		return smsList;
 	}
 
 	public static void main(String[] args) {
-		ArrayList<String> SMSList = new ArrayList<String>();
-		
 		try {
-			GoogleVoice googleVoice = new GoogleVoice();
-			for (int i = 0; i < 1; ++i) {
-				SMSList = googleVoice.getUnreadSMS();
+			List<SMSObject> smsList = GoogleVoice.getUnreadSMS();
+			System.out.println("Number of unread messages: " + smsList.size());
+			for(SMSObject sms : smsList){
+				System.out.println(sms.getFromNumber() + "\n" + sms.getMessage() + "\n" + sms.getTimestamp());
 			}
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			System.err.println(e.getMessage());
 		}
-		
-		System.out.println("Size of SMSList = "+SMSList.size());
 	}
 }
