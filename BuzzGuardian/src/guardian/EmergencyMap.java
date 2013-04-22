95779fbe8184fa2ef03b705b16c553319de0b98d
Sandeep Manchem <smanchem3@gatech.edu>
2013-04-22 00:09:55 -0400

    EmergencyMap java servlet prints the latest TimeStamp by querying the TrackingSMS table in Database. This value can be use as required.

--- BuzzGuardian/src/guardian/EmergencyMap.java
+++ BuzzGuardian/src/guardian/EmergencyMap.java
@@ -2,6 +2,11 @@ package guardian;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,9 +35,26 @@ public class EmergencyMap extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Timestamp ts = null;
+		String connectionURL = "jdbc:mysql://localhost:3306/buzz";
+		Connection connection = null;
+		try {
+	    	Class.forName("com.mysql.jdbc.Driver");
+	        connection = DriverManager.getConnection(connectionURL, "buzz",
+	                "guardian");
+	        Statement stmt = connection.createStatement();			
+	        String sql = "select TimeStamp from trackingsms order by ID desc LIMIT 1" ;
+	        ResultSet rs = stmt.executeQuery(sql);
+	        while (rs.next()) {
+		           ts = rs.getTimestamp(1);
+	        }
+	        connection.close();
+	    } catch (Exception e) {
+	    	System.out.println(e);
+	    }
 		response.setContentType("text/plain");
 		PrintWriter out = response.getWriter();
-		out.println("Emergency Map shall be displayed here!");
+		out.println("Latest TimeStamp = " + ts.toString());
 	}
 
 	/**
